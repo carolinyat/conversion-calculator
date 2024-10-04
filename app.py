304e73b58868825af54fcba15011e3ae8a1e5c8d
Caroliny Abreu <caroliny.abreu@healthbit.com.br>
2024-10-04 11:13:52 -0300

    initial commit - configurando

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Calculadora de Conversão - Backend em Flask"
+
+@app.route('/convert/temperature', methods=['GET'])
+def convert_temperature():
+    value = float(request.args.get('value'))
+    from_unit = request.args.get('from')
+    to_unit = request.args.get('to')
+
+    if from_unit == 'celsius' and to_unit == 'fahrenheit':
+        result = (value * 9/5) + 32
+    elif from_unit == 'fahrenheit' and to_unit == 'celsius':
+        result = (value - 32) * 5/9
+    else:
+        return jsonify({'error': 'Conversão não suportada'}), 400
+
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
